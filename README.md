fa893ee7150dcfbd82e94551bf972068f598aa07
Kotaimen <kotaimen.c@gmail.com>
2016-01-25 21:34:05 +0800

    Update README and squash.sh

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Stonemason Base Docker Image
 
-Base docker image for GIS packages, speeds up main docker image build.
+Base image of `stonemason`, based on `debian:jessie`.
+Custom built `mapnik` for possibly patching.
+
+Warning:  Build this image on a two core CPU requires about 8G memory.
+
+To reduce final image size use provided `squash.sh`.
+
+Download `docker-squash` from: https://github.com/jwilder/docker-squash .
+
+
+Tags:
+
+- `mapnik3.0.9`: Python2.7, Mapnik+3.0.9, Freetype 2.6.1
+
--- squash.sh
+++ squash.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Build image
-docker build -t stonemason-base:mapnik3.0.9 debian
+REPO_NAME=kotaimen
+IMAGE_TAG=stonemason-base:mapnik3.0.9
 
+# Build image
+docker pull debian:jessie
+docker build -t ${IMAGE_TAG} debian
+
+# Squash into a single layer, reduces image size
 # See http://jasonwilder.com/blog/2014/08/19/squashing-docker-images/
-docker save stonemason-base:mapnik3.0.9 | sudo TMPDIR=/var/run/shm docker-squash  -from root -t kotaimen/stonemason-base:mapnik3.0.9 -verbose | docker load
+docker save ${IMAGE_TAG} | sudo TMPDIR=/var/run/shm docker-squash  -from $(docker images -q debian:jessie) -t ${REPO_NAME}/${IMAGE_TAG} -verbose | docker load
