3231c9eecdf588d3f0154795d6a679e8b1005b94
Kotaimen <kotaimen.c@gmail.com>
2016-02-25 10:25:31 +0800

    Update freetype version to 2.6.3

--- squash.sh
+++ squash.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-REPO_NAME=kotaimen
-IMAGE_TAG=stonemason-base:mapnik3.0.9
-
-# Build image
-docker pull debian:jessie
-docker build -t ${IMAGE_TAG} debian
-
-# Squash into a single layer, reduces image size
+# Squash built images into a single layer, reduces image size.
 # See http://jasonwilder.com/blog/2014/08/19/squashing-docker-images/
-docker save ${IMAGE_TAG} | sudo TMPDIR=/var/run/shm docker-squash  -from $(docker images -q debian:jessie) -t ${REPO_NAME}/${IMAGE_TAG} -verbose | docker load
+
+#
+# Doesn't work under docker-1.10+
+#
+
+REPO_NAME=kotaimen
+IMAGE_TAG=stonemason-base:mapnik3.0.9-freetype2.6.3
+
+docker pull debian:jessie
+
+docker build -t ${IMAGE_TAG} debian
+docker save ${IMAGE_TAG} | sudo TMPDIR=/dev/shm docker-squash -from $(docker images -q debian:jessie) -t ${REPO_NAME}/${IMAGE_TAG} -verbose | docker load
